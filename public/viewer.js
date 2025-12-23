b2af02dfb7e0ded1f866b8e9c699611ff98ae11c
John Carroll <jcarroll@twomilesolutions.com>
2025-12-23 11:18:50 -0800

    Update viewer.js

--- public/viewer.js
+++ public/viewer.js
@@ -1,1 +1,6 @@
-need
+document.getElementById("loadLedger").onclick = async () => {
+  const res = await fetch("/ledger");
+  const data = await res.json();
+  document.getElementById("ledgerOutput").textContent =
+    JSON.stringify(data, null, 2);
+};
