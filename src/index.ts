6e45d9a258723f0cf18b04b61d2c24c48fde7fd9
John Carroll <jcarroll@twomilesolutions.com>
2025-12-23 11:47:58 -0800

    Create src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+{
+  "name": "soliton-north-star-node",
+  "version": "1.0.0",
+  "main": "dist/index.js",
+  "scripts": {
+    "build": "tsc",
+    "start": "NODE_ENV=production node dist/index.js",
+    "dev": "ts-node src/index.ts"
+  },
+  "dependencies": {
+    "express": "^4.19.0",
+    "cors": "^2.8.5",
+    "crypto-js": "^4.2.0",
+    "uuid": "^9.0.1",
+    "ws": "^8.16.0"
+  },
+  "devDependencies": {
+    "typescript": "^5.7.0",
+    "ts-node": "^10.9.2"
+  }
+}
